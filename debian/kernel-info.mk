aecb26bf464a6a186eb9b92308b53b855f128d89
berbascum <berbascum@ticv.cat>
2025-07-22 23:59:36 +0200

    (fragments) kernel-info: Add support for config extra fragments extended
    This commit does not enable any fragment, just adds the required
    vars as template
    
    Signed-off-by: berbascum <berbascum@ticv.cat>

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -30,6 +30,15 @@ DEVICE_FULL_NAME = Xiaomi POCO X3 Pro
 # If unsure, keep this to 0.
 KERNEL_CONFIG_USE_FRAGMENTS = 1
 
+# Enable kernel config device extra fragments (extended)
+KERNEL_CONFIG_USE_EXTRA_FRAGMENTS = 0
+KERNEL_CONFIG_EXTRA_FRAGMENTS = fragment.config
+## Development fragments
+KERNEL_CONFIG_USE_DEVEL_FRAGMENTS = 0
+KERNEL_CONFIG_DEVEL_FRAGMENTS = vayu-test.config vayu-develop.config
+KERNEL_CONFIG_USE_DEBUG_FRAGMENTS = 0
+KERNEL_CONFIG_DEBUG_FRAGMENTS = vayu-debug.config common_fragments/debug.config
+
 # Whether to use diffconfig to generate the device-specific configuration.
 # If you enable this, you should set KERNEL_CONFIG_USE_FRAGMENTS to 1.
 # If unsure, keep this to 0.
