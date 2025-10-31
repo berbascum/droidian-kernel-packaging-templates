24d35a7a75ab768f6032efc956ad0c6519debe6a
berbascum <berbascum@ticv.cat>
2025-10-31 04:41:45 +0100

    (cmdline) kernel-info: disable luks cust device
    Signed-off-by: berbascum <berbascum@ticv.cat>

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -15,7 +15,8 @@ KERNEL_BASE_VERSION = 4.14-0
 #KBUILD_DROIDIAN_VERSION = d0-rc0
 
 # The kernel cmdline to use
-KERNEL_BOOTIMAGE_CMDLINE = androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 console=tty0 systemd.unified_cgroup_hierarchy=0 luks.cust.dev=/dev/mmcblk0p4
+KERNEL_BOOTIMAGE_CMDLINE = androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 console=tty0 systemd.unified_cgroup_hierarchy=0
+# luks.cust.dev=/dev/mmcblk0p4
 # Add to avoid the lvm resize at the first boot
   # droidian.lvm.noresize
 # In development, early access available
