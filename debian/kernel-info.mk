015ae2165891d936499c15cea55f89f30c515fb3
berbascum <berbascum@ticv.cat>
2025-10-31 03:27:30 +0100

    (defconfig) kernel-info: enable debug fragments
    Add debug from common_fragments
    
    Signed-off-by: berbascum <berbascum@ticv.cat>

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -49,8 +49,9 @@ KERNEL_CONFIG_USE_EXTRA_FRAGMENTS = 0
 ## Development fragments
 KERNEL_CONFIG_USE_DEVEL_FRAGMENTS = 0
 # KERNEL_CONFIG_DEVEL_FRAGMENTS = vayu-test.config vayu-develop.config
-KERNEL_CONFIG_USE_DEBUG_FRAGMENTS = 0
-# KERNEL_CONFIG_DEBUG_FRAGMENTS = vayu-debug.config common_fragments/debug.config
+KERNEL_CONFIG_USE_DEBUG_FRAGMENTS = 1
+KERNEL_CONFIG_DEBUG_FRAGMENTS = common_fragments/debug.config
+#vayu-debug.config
 
 # Whether to use diffconfig to generate the device-specific configuration.
 # If you enable this, you should set KERNEL_CONFIG_USE_FRAGMENTS to 1.
