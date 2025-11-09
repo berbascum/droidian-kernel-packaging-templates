a1675d0d9197c06d688aac271704682ba20024df
berbascum <berbascum@ticv.cat>
2025-11-09 15:13:58 +0100

    (fragment) vayu-container: enable the fragment
    Signed-off-by: berbascum <berbascum@ticv.cat>

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -45,8 +45,7 @@ KERNEL_CONFIG_USE_FRAGMENTS = 1
 
 # Enable kernel config device extra fragments (extended)
 KERNEL_CONFIG_USE_EXTRA_FRAGMENTS = 1
-KERNEL_CONFIG_EXTRA_FRAGMENTS = vayu-boot.config
-# vayu-container.config
+KERNEL_CONFIG_EXTRA_FRAGMENTS = vayu-boot.config vayu-container.config
 ## Development fragments
 KERNEL_CONFIG_USE_DEVEL_FRAGMENTS = 0
 # KERNEL_CONFIG_DEVEL_FRAGMENTS = vayu-test.config vayu-develop.config
