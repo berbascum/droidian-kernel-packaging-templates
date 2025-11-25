0e0f3c7924a2489edc8d57b673d0f2ded5216daa
berbascum <berbascum@ticv.cat>
2025-11-25 14:14:38 +0100

    (defconfig) kernel-info: enable the vayu-halium fragment
    Currently just sets the localversion to halium
    
    Signed-off-by: berbascum <berbascum@ticv.cat>

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -52,7 +52,7 @@ KERNEL_CONFIG_USE_FRAGMENTS = 1
 
 # Enable kernel config device extra fragments (extended)
 KERNEL_CONFIG_USE_EXTRA_FRAGMENTS = 1
-KERNEL_CONFIG_EXTRA_FRAGMENTS = vayu-droidian.config vayu-boot.config vayu-security.config vayu-lxc.config vayu-network.config vayu-kvm.config vayu-container.config vayu-lto-disable.config
+KERNEL_CONFIG_EXTRA_FRAGMENTS = vayu-halium.config vayu-droidian.config vayu-boot.config vayu-security.config vayu-lxc.config vayu-network.config vayu-kvm.config vayu-container.config vayu-lto-disable.config
 ## Development fragments
 KERNEL_CONFIG_USE_DEVEL_FRAGMENTS = 0
 # KERNEL_CONFIG_DEVEL_FRAGMENTS = vayu-test.config vayu-develop.config
