3014b3d65d0f159f208482fa9c64b373482d440b
berbascum <berbascum@ticv.cat>
2025-12-08 16:57:52 +0100

    (fragments) kernel-info: disable vayu-debug extra fragment
    Signed-off-by: berbascum <berbascum@ticv.cat>

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -58,7 +58,8 @@ KERNEL_CONFIG_EXTRA_FRAGMENTS = vayu-lxc.config vayu-security.config vayu-camera
 KERNEL_CONFIG_USE_DEVEL_FRAGMENTS = 0
 # KERNEL_CONFIG_DEVEL_FRAGMENTS = vayu-test.config vayu-develop.config
 KERNEL_CONFIG_USE_DEBUG_FRAGMENTS = 1
-KERNEL_CONFIG_DEBUG_FRAGMENTS = common_fragments/debug.config vayu-debug.config
+KERNEL_CONFIG_DEBUG_FRAGMENTS = common_fragments/debug.config
+# vayu-debug.config
 
 # Whether to use diffconfig to generate the device-specific configuration.
 # If you enable this, you should set KERNEL_CONFIG_USE_FRAGMENTS to 1.
