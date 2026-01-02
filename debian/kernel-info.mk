f5f29a346bf24f87723fb287656accd63f4b938a
berbascum <berbascum@ticv.cat>
2026-01-02 04:31:53 +0100

    Revert "(cmdline) kernel-info: remove selinux= flag"
    This reverts commit d933dd9e61e678024e4333a9559033d7f6e351bf.
    
    The reverted commit was working on o Droidian snapshot 101 installation
    previous to the final 101 release.
    
    But enabling selinux in the cmdline on new fresh 101-final installations
    causes a broken display whet phosh starts.
    
    This commit adds the 'selinux' flag again to the cmdline
    and keeps it disabled for now.
    
    This seems fine, but some Android services like 'keystore'
    are crashing because SELinux is not loaded.
    
    Possible workarounds:
    - Try CONFIG_SECURITY_SELINUX_DISABLE=1
      Might be we can boot with SELinux disabled,
      and enable it later at runtime.
    - Try CONFIG_SECURITY_SELINUX_BOOTPARAM_VALUE=0

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -15,7 +15,7 @@ KERNEL_DEVELOPER_NAME = l4v3nx
 #KBUILD_DROIDIAN_VERSION = d0-rc0[-debug]
 
 # The kernel cmdline to use
-KERNEL_BOOTIMAGE_CMDLINE = androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 console=tty0 systemd.unified_cgroup_hierarchy=1 androidboot.selinux=permissive enforcing=0 buildvariant=userdebug cgroup_disable=pressure ramoops_memreserve=4M
+KERNEL_BOOTIMAGE_CMDLINE = androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 console=tty0 systemd.unified_cgroup_hierarchy=1 androidboot.selinux=permissive enforcing=0 selinux=0 buildvariant=userdebug cgroup_disable=pressure ramoops_memreserve=4M
 
 # luks.cust.dev=/dev/mmcblk0p4
 # Add to avoid the lvm resize at the first boot
